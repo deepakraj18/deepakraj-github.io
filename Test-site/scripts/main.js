9b1de02d4c54eed15822a7ca3583130029d53a2d
depaksampath <68414880+deepakraj18@users.noreply.github.com>
2020-07-17 15:12:53 +0530

    Add files via upload

---
+++ Test-site/scripts/main.js
@@ -0,0 +1,16 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Ninja is as fast as you might think, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Ninja is as fast as you might think, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
